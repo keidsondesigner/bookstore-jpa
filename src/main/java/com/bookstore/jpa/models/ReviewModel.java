bbfd378156ef425bd6305b7a110c7c5eba53d496
Keidson Roby <keidsondesigner@gmail.com>
2024-06-30 14:03:26 -0400

    feat: Criado ReviewModel

---
+++ src/main/java/com/bookstore/jpa/models/ReviewModel.java
@@ -0,0 +1,45 @@
+package com.bookstore.jpa.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_review")
+public class ReviewModel implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Column(nullable = false)
+  private String comment;
+
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+}
